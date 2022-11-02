c8d8440fd25ce60992f3edd9d210349b33890e12
Denly09 <denniely.garcia09@gmail.com>
2022-11-02 19:22:50 +0800

    add some refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.includes(:user).all
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(params[:post].permit(:title, :content, :address))
+    @post = Post.new(post_params)
     @post.user = current_user
     if @post.save
       redirect_to posts_path
@@ -28,8 +29,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
-    if @post.update(params.require(:post).permit(:title, :content))
+    if @post.update(post_params)
       redirect_to posts_path
     else
       render :edit, status: :unprocessable_entity
@@ -37,9 +37,17 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path
   end
 
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :address)
+  end
 end
