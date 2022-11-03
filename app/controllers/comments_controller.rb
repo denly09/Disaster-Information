8c8804a81a97b5543d7f82ddd9d51dca8dd70f67
Denly09 <denniely.garcia09@gmail.com>
2022-11-03 13:53:32 +0800

    comment new, create and add _form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,29 @@
 class CommentsController < ApplicationController
   before_action :set_post
+  before_action :set_comment, only: [:edit, :update]
   def index
     @comments = @post.comments
   end
 
+  def new
+    @comment = @post.comments.build
+  end
+
+  def create
+    @comment = @post.comments.build(params_comment)
+    if @comment.save
+      redirect_to post_comments_path(@post)
+    else
+      render :new
+    end
+  end
   private
 
   def set_post
     @post = Post.find params[:post_id]
+  end
+
+  def params_comment
+    params.require(:comment).permit(:content)
   end
 end
