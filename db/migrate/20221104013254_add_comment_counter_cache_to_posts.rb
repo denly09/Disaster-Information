cf19c24766306a262e95af75d9be721c260b8276
Denly09 <denniely.garcia09@gmail.com>
2022-11-04 18:13:47 +0800

    add default comment_counts

--- db/migrate/20221104013254_add_comment_counter_cache_to_posts.rb
+++ db/migrate/20221104013254_add_comment_counter_cache_to_posts.rb
@@ -1,5 +1,5 @@
 class AddCommentCounterCacheToPosts < ActiveRecord::Migration[7.0]
   def change
-    add_column :posts, :comments_count, :integer
+    add_column :posts, :comments_count, :integer, default: 0
   end
 end
