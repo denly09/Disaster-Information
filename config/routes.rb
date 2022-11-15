5e4e56660ac7fe539d5ccabd1059ea41c56f3b8a
Denly09 <denniely.garcia09@gmail.com>
2022-11-15 19:28:32 +0800

    Done api barangay

--- config/routes.rb
+++ config/routes.rb
@@ -14,10 +14,14 @@ Rails.application.routes.draw do
   namespace :api do
     resources :regions, only: :index, defaults: { format: :json } do
       resources :provinces, only: :index, defaults: { format: :json } do
-        resources :city_municipalities, only: :index, defaults: {format: :json}
+        resources :city_municipalities, only: :index, defaults: {format: :json} do
+          resources :barangays, only: :index, defaults: {format: :json}
+        end
       end
       resources :districts, only: :index, defaults: { format: :json } do
-        resources :city_municipalities, only: :index, defaults: {format: :json}
+        resources :city_municipalities, only: :index, defaults: {format: :json} do
+          resources :barangays, only: :index, defaults: {format: :json}
+        end
       end
     end
   end
