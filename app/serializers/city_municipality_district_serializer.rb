21b7159f01e4ecb41a81a0bb04e67e9f7b62c3e3
Denly09 <denniely.garcia09@gmail.com>
2022-11-22 14:07:29 +0800

    remove files

--- app/serializers/city_municipality_district_serializer.rb
+++
@@ -1,7 +0,0 @@
-class CityMunicipalityDistrictSerializer < ActiveModel::Serializer
-  attributes :name, :district, :id
-
-  def district
-    object.district.name
-  end
-end
--- app/serializers/city_municipality_province_serializer.rb
+++
@@ -1,7 +0,0 @@
-class CityMunicipalityProvinceSerializer < ActiveModel::Serializer
-  attributes :name, :province, :id
-
-  def province
-    object.province.name
-  end
-end
