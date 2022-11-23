d14b627b0cfe00061243cd484dbe13b60e85a128
Denly09 <denniely.garcia09@gmail.com>
2022-11-23 15:59:56 +0800

    add constraint for admin and client

--- config/routes.rb
+++ config/routes.rb
@@ -3,31 +3,33 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-
   root "posts#index"
-
-  resources :posts do
-    resources :comments, expect: :show
+  constraints(ClientDomainConstraint.new) do
+    resources :posts do
+      resources :comments, except: :show
+    end
   end
   resources :categories
-  namespace :admin do
-    resources :users
+  constraints(AdminDomainConstraint.new) do
+    scope module: :admin, path: '/' do
+      resources :users
+      resources :posts
+    end
   end
+  get '/:unique_num', to: 'posts#short_url'
   namespace :api do
     resources :regions, only: :index, defaults: { format: :json } do
       resources :provinces, only: :index, defaults: { format: :json } do
-        resources :city_municipalities, only: :index, defaults: {format: :json} do
-          resources :barangays, only: :index, defaults: {format: :json}
+        resources :city_municipalities, only: :index, defaults: { format: :json } do
+          resources :barangays, only: :index, defaults: { format: :json }
         end
       end
       resources :districts, only: :index, defaults: { format: :json } do
-        resources :city_municipalities, only: :index, defaults: {format: :json} do
-          resources :barangays, only: :index, defaults: {format: :json}
+        resources :city_municipalities, only: :index, defaults: { format: :json } do
+          resources :barangays, only: :index, defaults: { format: :json }
         end
       end
     end
   end
-
-  get ':unique_num', to: 'posts#short_url'
 end
 
